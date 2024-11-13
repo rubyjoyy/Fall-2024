ce20ad6bccbe63dab2fc22d22b0a2987f2bc5967
rubyjoyy <ruby.valasek@gmail.com>
2024-11-13 10:58:44 -0600

    update

--- JS/form.js
+++ JS/form.js
@@ -1,19 +1,57 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
-    'use strict'
+console.clear(); 
+
+//mask phone number
+  var phoneVar = document.querySelector('#phone');
   
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
+  if (phoneVar) {
   
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
+      phoneVar.addEventListener('input', function (e) {
+        var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+        e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+      });
+  
+  }
+//mask phone number
+
+
+////show/hide///////////
+
+
+
+///////////////checkbox///////////////
+var checkBoxVar = document.querySelector('#morning');  
+
+if (checkBoxVar) {
+  
+  //just the button variable
+  let submit = document.getElementById("submit");
+  submit.addEventListener("click", chckcheckBox);
+
+  function chckcheckBox() {
+    
+    var checkBoxFieldChecked = document.querySelectorAll(".checkBox:checked"); // get the ones checked
+      var checkBoxField = document.querySelectorAll(".checkBox");
+    
+      /*change to checkBoxField and add .value*/
+      for (var myCounter = 0; myCounter < checkBoxFieldChecked.length; myCounter++) {
+        //for each checkbox that is checked, the counter is no longer zero
+      }
+
+      //check the counter
+      if (myCounter < 1) {
+        //alert('test');
+
+        alert("At leat one item must be checked from the checkboxes");
+        for (var checkBoxFieldCounter = 0; checkBoxFieldCounter < checkBoxField.length; checkBoxFieldCounter++) {     
+          checkBoxField[checkBoxFieldCounter].required = true; //make both required  
         }
-  
-        form.classList.add('was-validated')
-      }, false)
-    })
-  })()
+
+      }
+      else {
+
+        for (var checkBoxFieldCounter = 0; checkBoxFieldCounter < checkBoxField.length; checkBoxFieldCounter++) {     
+          checkBoxField[checkBoxFieldCounter].required = false;  //make both not required
+        }
+
+      }
+    } 
